62cca297caa9e6a320fc8cb37b4239111338b182
Amabange97 <amabangejesse@gmail.com>
2020-08-13 16:51:24 +0000

    --- Auto-Git Commit ---

--- js/math.js
+++ js/math.js
@@ -8,8 +8,13 @@ document.getElementById("start-game").onclick = function startNow(){
     if(playing == true){
         location.reload();
     }
-    else{
+    else{ //if we are not playing
+        playing = true;
+        alert(playing);
         score = 0;
         document.getElementById("timeremaining").style.display="block";
+        // document.getElementById("timevalue").innerHTML=score;
+        document.getElementById("scoreValue").innerHTML=score;
+        document.getElementById("start-game").innerHTML="Reset Game";
     }
 }
