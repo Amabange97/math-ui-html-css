632487fa3b10addf48b11f23c27b15f9e2702c98
Amabange97 <amabangejesse@gmail.com>
2020-08-13 17:21:30 +0000

    --- Auto-Git Commit ---

--- js/math.js
+++ js/math.js
@@ -1,20 +1,47 @@
+
+var playing = false;
+var score;
+var action;
+var timeValue;
 document.getElementById("start-game").onclick = function startNow(){
    
 
 
-    var playing = false;
-    var score;
 
     if(playing == true){
         location.reload();
     }
     else{ //if we are not playing
         playing = true;
-        alert(playing);
         score = 0;
         document.getElementById("timeremaining").style.display="block";
-        // document.getElementById("timevalue").innerHTML=score;
+        timeValue=3;
+        document.getElementById("timevalue").innerHTML=timeValue;
         document.getElementById("scoreValue").innerHTML=score;
         document.getElementById("start-game").innerHTML="Reset Game";
+
+
+        //startCounter
+        startCounter();
+
+
+       
+
+    }
+}
+function startCounter() {
+    action = setInterval(function () {
+        timeValue -= 1;
+        document.getElementById("timevalue").innerHTML = timeValue;
+        if (timeValue == 0) {
+            stopCount();
+            document.getElementById("game-over").style.display="block";
+            document.getElementById("game-over").innerHTML="<p>Game Over</p> <p>Your Score is "+ score +"</p>";
+            document.getElementById("timeremaining").style.display="none";
+        }
+
+    }, 1000);
+    function stopCount() {
+        clearInterval(action);
     }
 }
