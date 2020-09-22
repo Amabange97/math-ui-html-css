516d049d158ee1ee7a62021d3d7d3307efbcf909
Amabange97 <amabangejesse@gmail.com>
2020-09-22 17:18:07 +0000

    --- Auto-Git Commit ---

--- fruitGame/js/jquery.js
+++ fruitGame/js/jquery.js
@@ -1,6 +1,7 @@
 var playing =false;
 var score;
 var trialsLeft;
+var action,step;
 var fruits=['apple','banana','cherries','grapes','mango','orange','peach','pear','watermelon'];
 $("#startreset").click(
     function(){
@@ -31,6 +32,15 @@ function startAction(){
     $("#fruit1").show();
     chooseFruit();
     $("#fruit1").css({ 'left': Math.round(550 * Math.random()), 'top': -50 });
+    //define step
+    step = 1 + Math.round(5 * Math.random());
+    
+    //action move fruit down by 10ms
+    action= setInterval(
+        function (){
+            $("#fruit1").css('top', $("#fruit1").position().top + step);
+        },10
+    );
 }
 function chooseFruit(){
     $("#fruit1").attr('src', 'images/' + fruits[Math.round(8 * Math.random())] + '.png');
